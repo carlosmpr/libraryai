7e3023a8f161b40d3d94c8261e0696393c280d04
Carlos Polanco <carlospolanco@Carloss-MacBook-Air.local>
2024-07-30 18:16:41 -0400

    build test22

--- client/src/components/ui/PopUp.jsx
+++ client/src/components/ui/PopUp.jsx
@@ -1,7 +1,6 @@
-
-import { useState, useEffect } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
-import { XMarkIcon } from '@heroicons/react/24/outline';
+import { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { XMarkIcon } from "@heroicons/react/24/outline";
 
 const Popup = ({
   popupId,
@@ -15,9 +14,9 @@ const Popup = ({
   successMessage,
   errorMessage,
   children,
-  customStyle = 'modal-box',
+  customStyle = "modal-box",
   onSuccess,
-  progress
+  progress,
 }) => {
   const [showMessage, setShowMessage] = useState(false);
 
@@ -36,10 +35,10 @@ const Popup = ({
       }
     };
 
-    dialog.addEventListener('close', handleClose);
+    dialog.addEventListener("close", handleClose);
 
     return () => {
-      dialog.removeEventListener('close', handleClose);
+      dialog.removeEventListener("close", handleClose);
     };
   }, [popupId, isOpen, onClose]);
 
@@ -60,31 +59,33 @@ const Popup = ({
 
   return (
     <dialog id={popupId} className="modal">
-      <div className={`relative bg-base-200 border border-2 border-black shadow-2xl shadow-white/40 ${customStyle}`}>
+      <div
+        className={`relative bg-base-200 border border-2 border-black shadow-2xl shadow-white/40 ${customStyle}`}
+      >
         <div className="text-center my-10 space-y-1">
           <h2 className="font-bold text-3xl">{title}</h2>
           <p className="text-base-content/70 text-sm">{description}</p>
         </div>
-        <div className="relative">
+        <div className="relative h-full flex items-center justify-center">
           <AnimatePresence>
             {isLoading && (
               <motion.div
                 key="loading"
-                initial={{ opacity: 0, x: '-100%' }}
+                initial={{ opacity: 0, x: "-100%" }}
                 animate={{ opacity: 1, x: 0 }}
-                exit={{ opacity: 0, x: '100%' }}
+                exit={{ opacity: 0, x: "100%" }}
                 transition={{ duration: 0.5 }}
-                className="absolute inset-0 flex items-center justify-center z-10 h-full"
+                className="absolute inset-0 flex flex-col items-center justify-center z-10"
               >
-             {progress !== undefined && (
-                  <div className="absolute bottom-4 left-0 right-0 flex justify-center">
-                    <p className="bg-gray-800 text-white text-8xl px-3 py-1 rounded-md">
-                      {progress}%
+                {progress !== undefined && (
+                  <div className="flex justify-center mb-4">
+                    <p className="bg-gray-800 text-white text-2xl px-3 py-1 rounded-md">
+                      {Math.round(progress)}% {/* Round the progress to the nearest integer */}
                     </p>
                   </div>
                 )}
-                
-                <div className="flex">
+
+                <div className="flex items-center justify-center space-x-2">
                   <span className="loading loading-dots loading-lg"></span>
                   <span className="loading loading-dots loading-lg"></span>
                   <span className="loading loading-dots loading-lg"></span>
@@ -97,13 +98,17 @@ const Popup = ({
             {showMessage && (
               <motion.div
                 key="message"
-                initial={{ opacity: 0, x: '-100%' }}
+                initial={{ opacity: 0, x: "-100%" }}
                 animate={{ opacity: 1, x: 0 }}
-                exit={{ opacity: 0, x: '100%' }}
+                exit={{ opacity: 0, x: "100%" }}
                 transition={{ duration: 0.5 }}
                 className="absolute inset-0 flex items-center justify-center bg-opacity-75 z-10"
               >
-                <div className={`message ${isSuccess ? 'text-green-500' : 'text-red-500'}`}>
+                <div
+                  className={`message ${
+                    isSuccess ? "text-green-500" : "text-red-500"
+                  }`}
+                >
                   {isSuccess ? successMessage : errorMessage}
                 </div>
               </motion.div>
@@ -122,7 +127,11 @@ const Popup = ({
             </motion.div>
           )}
         </div>
-        <button type="button" className="absolute top-5 right-10" onClick={onClose}>
+        <button
+          type="button"
+          className="absolute top-5 right-10"
+          onClick={onClose}
+        >
           <XMarkIcon className="w-6" />
         </button>
       </div>
